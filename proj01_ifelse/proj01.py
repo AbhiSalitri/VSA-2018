7d7f20859f3fd24e6a4ca946480780b7a4ebe346
AbhiSalitri <39976456+AbhiSalitri@users.noreply.github.com>
2018-06-11 14:32:32 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -12,4 +12,25 @@
 # Then, it prints a sentence that says the number of days and months until their birthday
 
 
-# If you complete extensions, describe your extensions here!
+# If you complete extensions, describe your extensions her
+
+# print "Hi, my name is Abhi."
+# name = raw_input("What is your name?")
+# name = name[0].upper() + name[1:].lower()
+# grade = str(raw_input("What grade are you in?"))
+# grade = grade[0]
+# time = 12 - int(grade)
+# print name, "you will graduate from high school in " + str(time) + " years."
+
+current_month = 6
+current_day = 11
+
+month = raw_input("What month were you born in? (use the number)")
+day = raw_input("What day were you born on?")
+
+if month >= current_month:
+
+   month1 = 12 -(month - current_month)
+else:
+    day >= current_day
+    day1 = 12 -(day - current_day)
