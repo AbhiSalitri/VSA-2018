1e07631b068807e31441073c2f45fc8f9ceb6b3d
AbhiSalitri <39976456+AbhiSalitri@users.noreply.github.com>
2018-06-11 15:39:13 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -1,26 +1,26 @@
 # Name:
 # Date:
-
+#
 # proj01: A Simple Program
-
+#
 # Part I:
 # This program asks the user for his/her name and grade.
-#Then, it prints out a sentence that says the number of years until they graduate.
-
+# Then, it prints out a sentence that says the number of years until they graduate.
+#
 # Part II:
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
-
-
+#
+#
 # If you complete extensions, describe your extensions her
 
-# print "Hi, my name is Abhi."
-# name = raw_input("What is your name?")
-# name = name[0].upper() + name[1:].lower()
-# grade = str(raw_input("What grade are you in?"))
-# grade = grade[0]
-# time = 12 - int(grade)
-# print name, "you will graduate from high school in " + str(time) + " years."
+print "Hi, my name is Abhi."
+name = raw_input("What is your name?")
+name = name[0].upper() + name[1:].lower()
+grade = str(raw_input("What grade are you in?"))
+grade = grade[0]
+time = 12 - int(grade)
+print name, ",you will graduate from high school in " + str(time) + " years."
 
 current_month = 6
 current_day = 11
@@ -41,4 +41,4 @@ else:
         month1 = 11
     else:
         month1 = month1 - 1
-print "Your birthday will be in " + str(month1) + " months and " + str(day1) + " days."
+print str(name) + ",your birthday will be in " + str(month1) + " months and " + str(day1) + " days."
