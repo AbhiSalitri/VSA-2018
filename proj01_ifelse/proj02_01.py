926e440a50ce1bb529301ad3baa1336a617285a0
AbhiSalitri <39976456+AbhiSalitri@users.noreply.github.com>
2018-06-12 11:09:39 -0500

    Add files via upload

---
+++ proj01_ifelse/proj02_01.py
@@ -0,0 +1,26 @@
+# Name:
+# Date:
+
+# proj02: sum
+
+# Write a program that prompts the user to enter numbers, one per line,
+# ending with a line containing 0, and keep a running sum of the numbers.
+# Only print out the sum after all the numbers are entered
+# (at least in your final version). Each time you read in a number,
+# you can immediately use it for your sum,
+# and then be done with the number just entered.
+
+#Example:
+# Enter a number to sum, or 0 to indicate you are finished: 4
+# Enter a number to sum, or 0 to indicate you are finished: 5
+# Enter a number to sum, or 0 to indicate you are finished: 2
+# Enter a number to sum, or 0 to indicate you are finished: 10
+# Enter a number to sum, or 0 to indicate you are finished: 0
+#The sum of your numbers is: 21
+input = 1
+sum = 0
+
+while   input != 0:
+    input = int(raw_input("Enter a number, or enter 0 to indicate that you are done."))
+    sum = sum + input
+print "The sum of your numbers is " + str(sum) + "."
