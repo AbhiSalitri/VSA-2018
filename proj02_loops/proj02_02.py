939321890188b10c6a9879133408356d4bfcc01a
AbhiSalitri <39976456+AbhiSalitri@users.noreply.github.com>
2018-06-12 13:54:33 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -10,4 +10,13 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 
+previous_number = 0
+current_number = 1
+next_number = 1
 
+x = int(raw_input("How many Fibonacci numbers would you like to generate?"))
+for number in range (0, x):
+    print current_number
+    next_number = previous_number + current_number
+    previous_number = current_number
+    current_number = next_number
