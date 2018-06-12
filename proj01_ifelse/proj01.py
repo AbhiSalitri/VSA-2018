00079abec7f2680b943bc32b769a403214958dab
AbhiSalitri <39976456+AbhiSalitri@users.noreply.github.com>
2018-06-12 14:58:10 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -24,39 +24,13 @@ print name,",you will graduate from high school in " + str(time) + " years."
 
 current_month = raw_input("What is the current month? (use the number)")
 current_day = raw_input("What is the current day?")
-month = raw_input("What month were you born in? (use the word)")
-month = month[0].upper() + month[1:].lower()
+month = raw_input("What month were you born in? (use the number)")
 day = raw_input("What day were you born on?")
-if month == "January":
-    month = 1
-elif month == "February":
-    month = 2
-elif month == "March":
-    month = 3
-elif month == "April":
-    month = 4
-elif month == "May":
-    month = 5
-elif month == "June":
-    month = 6
-elif month == "July":
-    month = 7
-elif month == "August":
-    month = 8
-elif month == "September":
-    month = 9
-elif month == "October":
-    month = 10
-elif month == "November":
-    month = 11
-else:
-    month = int(12)
-
 if int(month) >= int(current_month):
    month1 = (int(month) - int(current_month))
 else:
     month1 = int(12 - (int(current_month) - int(month)))
-if int(day) >= current_day:
+if int(day) >= int(current_day):
     day1 = (int(day) - int(current_day))
 else:
     day1 = int(30 - (int(current_day) - int(day)))
